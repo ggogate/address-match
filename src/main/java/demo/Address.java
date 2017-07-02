3805863b77f95ce333add182ef9950f2f6df6dc5
Gaurav Gogate <gaurav.gogate@gmail.com>
2017-07-02 22:16:49 +0530

    updated Address object

---
+++ src/main/java/demo/Address.java
@@ -0,0 +1,106 @@
+package demo;
+
+public class Address {
+	
+	private String addressType;
+	private String addressLine1;
+	private String addressLine2;
+	private String addressLine3;
+	private String addressLine4;
+	private String streetName;
+	private String locality;
+	private String country;
+	private String region;
+	private String city;
+	private String metroArea;
+	private String state;
+	private String zip;
+	private String zip4;
+	
+	
+	public String getAddressType() {
+		return addressType;
+	}
+	public void setAddressType(String addressType) {
+		this.addressType = addressType;
+	}
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	public String getAddressLine3() {
+		return addressLine3;
+	}
+	public void setAddressLine3(String addressLine3) {
+		this.addressLine3 = addressLine3;
+	}
+	public String getAddressLine4() {
+		return addressLine4;
+	}
+	public void setAddressLine4(String addressLine4) {
+		this.addressLine4 = addressLine4;
+	}
+	public String getStreetName() {
+		return streetName;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	public String getLocality() {
+		return locality;
+	}
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getMetroArea() {
+		return metroArea;
+	}
+	public void setMetroArea(String metroArea) {
+		this.metroArea = metroArea;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getZip4() {
+		return zip4;
+	}
+	public void setZip4(String zip4) {
+		this.zip4 = zip4;
+	}
+
+}
---
+++ src/test/java/demo/tests/AddressMatchTest2.java
@@ -0,0 +1,23 @@
+package demo.tests;
+
+import demo.Address;
+import demo.AddressHelper;
+import demo.AddressMatcher;
+
+public class AddressMatchTest2 {
+
+	public static void main(String[] args) {
+		
+		Address a1 = new Address();
+		
+		a1.setState("AZ");
+		a1.setZip("89999");
+		a1.setCountry("US");
+		AddressMatcher am = new AddressMatcher();
+		AddressHelper ah = new AddressHelper();
+		am.setHelper(ah);
+		//System.out.println(am.compare(s1, s2));
+
+	}
+
+}
